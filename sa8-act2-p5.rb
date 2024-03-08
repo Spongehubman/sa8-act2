2ff3e1a6fc0e07f1b729e0aaa2a423ca8fa32981
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-07 19:31:15 -0600

    Work on 'sa8-act2-p5.rb' has started officially.

--- sa8-act2-p5.rb
+++ sa8-act2-p5.rb
@@ -1,2 +1,31 @@
 
+# This program demonstrates that a namespace module can be used to operate with classes inside.
 
+module Payments
+
+    class Invoice
+        attr_accessor id:, amount:
+
+        def initialize(id, amount)
+            @id = id
+            @amount = amount
+        end
+
+    end
+
+    class Receipt
+        attr_accessor id:, amount:
+
+        def initialize(id, amount)
+            @id = id
+            @amount = amount
+        end
+
+    end
+end
+
+# "i1" marks the creation of a new "Invoice" instance, by using a namespace reference call.
+i1 = Payments::Invoice.New("Inv:0135", 2300)
+
+# "r1" marks the creation of a new "Receipt" instance, by using a namespace reference call.
+r1 = Payments::Receipt.New("Rec:0135", 3300)
